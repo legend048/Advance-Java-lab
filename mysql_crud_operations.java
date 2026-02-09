b658e9e4272a3ff94343dc15c595ec3ab77f00ba
legend048 <pklegend048@gmail.com>
2026-02-09 11:27:55 +0530

    qwe

--- mysql_crud_operations.java
+++ mysql_crud_operations.java
@@ -8,13 +8,13 @@ class mysql_con_and_print_emp{
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS emp(id INT PRIMARY KEY, name VARCHAR(50), position VARCHAR(50))");
             stmt.executeUpdate("INSERT INTO emp VALUES(1, 'John Doe', 'Manager')");
             rs=stmt.executeQuery("select * from emp");
-            while(rs.next()) System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            while(rs.next()) System.out.println(String.format("ID: %d | Name: %s | Position: %s", rs.getInt(1), rs.getString(2), rs.getString(3)));
             stmt.executeUpdate("UPDATE emp SET name='Jane Smith' WHERE id=1");
             rs=stmt.executeQuery("select * from emp");
-            while(rs.next()) System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            while(rs.next()) System.out.println(String.format("ID: %d | Name: %s | Position: %s", rs.getInt(1), rs.getString(2), rs.getString(3)));
             stmt.executeUpdate("DELETE FROM emp WHERE id=1");
             rs=stmt.executeQuery("select * from emp");
-            while(rs.next()) System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            while(rs.next()) System.out.println(String.format("ID: %d | Name: %s | Position: %s", rs.getInt(1), rs.getString(2), rs.getString(3)));
             con.close(); 
         }
         catch(Exception e){ System.out.println(e);}
